30258f168d805ff2d6a92afaf23c90af88b27d11
Erick Flores <ferick8246@gmail.com>
2021-08-05 22:41:00 -0500

    update code

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -7,8 +7,8 @@ import {
   UPDATE_CATEGORIES,
   UPDATE_CURRENT_CATEGORY,
   CLEAR_CART,
-  TOGGLE_CART,
-} from './actions';
+  TOGGLE_CART
+} from "./actions";
 
 const initialState = {
   products: [],
@@ -16,11 +16,10 @@ const initialState = {
   currentCategory: '',
   cart: [],
   cartOpen: false
-}
+};
 
 export const reducers = (state = initialState, action) => {
   switch (action.type) {
-   
     case UPDATE_PRODUCTS:
       return {
         ...state,
@@ -33,48 +32,47 @@ export const reducers = (state = initialState, action) => {
         cartOpen: true,
         cart: [...state.cart, action.product],
       };
+
     case ADD_MULTIPLE_TO_CART:
       return {
         ...state,
         cart: [...state.cart, ...action.products],
       };
-    
+
     case UPDATE_CART_QUANTITY:
       return {
         ...state,
         cartOpen: true,
-        cart: state.cart.map((product) => {
+        cart: state.cart.map(product => {
           if (action._id === product._id) {
-            product.purchaseQuantity = action.purchaseQuantity;
+            product.purchaseQuantity = action.purchaseQuantity
           }
-          return product;
-        }),
+          return product
+        })
       };
 
-    
     case REMOVE_FROM_CART:
-      let newState = state.cart.filter((product) => {
+      let newState = state.cart.filter(product => {
         return product._id !== action._id;
       });
 
-      
       return {
         ...state,
         cartOpen: newState.length > 0,
-        cart: newState,
+        cart: newState
       };
 
     case CLEAR_CART:
       return {
         ...state,
         cartOpen: false,
-        cart: [],
+        cart: []
       };
 
     case TOGGLE_CART:
       return {
         ...state,
-        cartOpen: !state.cartOpen,
+        cartOpen: !state.cartOpen
       };
 
     case UPDATE_CATEGORIES:
@@ -86,10 +84,9 @@ export const reducers = (state = initialState, action) => {
     case UPDATE_CURRENT_CATEGORY:
       return {
         ...state,
-        currentCategory: action.currentCategory,
-      };
+        currentCategory: action.currentCategory
+      }
 
-    
     default:
       return state;
   }
